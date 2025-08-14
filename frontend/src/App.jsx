cdb63f06e1e0c7373c064e56b11d9284b3ec4434
Steven Mendoza <smenthor2005@gmail.com>
2025-08-13 21:23:24 -0600

    chore(Change App Routes & index meta)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import MainLayout from "./layouts/MainLayout";
+import { Navigate } from "react-router-dom";
 
 const Landing = lazy(() => import("./pages/Landing"));
 const Home = lazy(() => import("./pages/Home"));
@@ -12,25 +13,18 @@ function App() {
       <Route element={<MainLayout />}>
         <Route path="/" element={
             <Suspense fallback={<div></div>}>
-              <Landing />
-            </Suspense> 
-        } 
-        />
-        
-        <Route path="/home" element={
-            <Suspense fallback={<div></div>}>
               <Home />
             </Suspense>
           }
         />
-
-        <Route path="*" element={
-            <Suspense fallback={<div></div>}> 
-            <NotFound />
-            </Suspense>
-          }
+        
+        <Route path="/style" element={
+            <Suspense fallback={<div></div>}>
+              <Landing />
+            </Suspense> 
+        } 
         />
-
+       <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
   );
